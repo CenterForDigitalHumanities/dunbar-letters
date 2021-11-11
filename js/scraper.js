63f66bedbfb325feb95e860b809a1921f6ae765b
Patrick Cuba <cubap@slu.edu>
2021-11-11 15:17:20 -0600

    limiting

--- js/scraper.js
+++ js/scraper.js
@@ -1,3 +1,6 @@
+import { default as DEER } from './deer-config.js'
+import pLimit from './plimit.js'
+
 const LOC = 'http://tinypaul.rerum.io/dla/proxy?url=https://udspace.udel.edu/rest/collections/599?expand=items&limit=800'
 const cacheLOC = 'media/udel.xml'
 
@@ -56,7 +59,68 @@ function drawItems(itemArray) {
 }
 
 function saveItems() {
-    return itemArray
+    const limiter = pLimit(4)
+    document.querySelectorAll("[is='record']").forEach(el => {
+        limiter(()=>hasRecord(el.metadata.url)
+        .then(existingData=>{
+
+            if(!existingData){
+                const anno = Object.assign({
+                target:el.metadata.url,
+                body: el.metadata
+            },DC_ROOT_ANNOTATION)
+            limiter(()=>fetch(DEER.URLS.CREATE, {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify(anno)
+            })
+            .then(res => res.ok ? el.querySelector("result").innerHTML = "matched!" : Promise.reject(res))
+                .catch(err => console.error(err))
+                )
+                const record = Object.assign({
+                    label:el.metadata.title
+                },NEW_RECORD)
+                limiter(()=>fetch(DEER.URLS.CREATE,{
+                    method: 'POST',
+                    mode: 'cors',
+                    body: JSON.stringify(record)
+                }))
+                .then(res => res.ok ? res.headers.get("location") ?? res.headers.get("Location") : Promise.reject(res))
+                .then(loc=>{
+                    el.setAttribute("dla-id",loc)
+                    const handleAnno = Object.assign({
+                        target:el.metadata.url,
+                        body: {
+                            url: { value: loc }
+                        }
+                    },DC_ROOT_ANNOTATION)
+                    limiter(()=>fetch(DEER.URLS.CREATE, {
+                        method: 'POST',
+                        mode: 'cors',
+                        body: JSON.stringify(anno)
+                    })
+                })
+                .catch(err => console.error(err))
+            }
+            
+        })
+        })
+        return true
+        
+        async function hasRecord(handle){
+        const historyWildcard = { $exists: true, $type: 'array', $eq: [] }
+        const query = {
+            target: handle,
+            '__rerum.history.next': historyWildcard
+        }
+        return fetch(DEER.URLS.QUERY, {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(query)
+        })
+        .then(res => res.ok ? res.json() : Promise.reject(res))
+        .then(results=>results.length>0)
+    }
 }
 
 function setMetadata(el, metadata) {
