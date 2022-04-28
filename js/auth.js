18f8eb8110b592db7cf4bb5b701f016027836f30
cubap <cubap@slu.edu>
2022-04-28 09:23:54 -0500

    documention

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,15 @@
+/**
+ * @module AuthButton Adds custom element for login/logout of Auth0, based on configuration below.
+ * @author cubap
+ * 
+ * @description This module includes a custom `<button is="auth-button">` element for authentication within 
+ * the Dunbar Public Library and Archive Project.
+ * Notes: 
+ * - Include this module and a button[is='auth-button'] element to use. 
+ * - Add the `disabled` property on any page that should be available to the public, but knowing the user may be helpful.
+ * - This can be made more generic by passing in the constants and parameterizing {app:'dla'}.
+ */
+
 import 'https://cdn.auth0.com/js/auth0/9.19.0/auth0.min.js'
 
 const AUDIENCE = "https://cubap.auth0.com/api/v2/"
@@ -16,6 +28,7 @@ const webAuth = new auth0.WebAuth({
 })
 
 const logout = () => webAuth.logout({ returnTo: origin })
+const login = () => webAuth.authorize({ authParamsMap: { 'app': 'dla' } })
 
 const getReferringPage = () => {
     try {
@@ -35,7 +48,7 @@ class AuthButton extends HTMLButtonElement {
         webAuth.checkSession({}, (err, result) => {
             if (err) {
                 if (this.getAttribute('disabled') !== null) { return }
-                webAuth.authorize({ authParamsMap: { 'app': 'dla' } })
+                login()
             }
             const ref = getReferringPage()
             if (ref && ref !== location.href) { location.href = ref }
